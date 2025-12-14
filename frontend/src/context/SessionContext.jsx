337d9b56233bc7b17b15b13901c5ce2fe3a90bcb
Abhi7482 <abhisheksonar7482@gmail.com>
2025-12-14 23:44:21 +0530

    Revert expiry change

--- frontend/src/context/SessionContext.jsx
+++ frontend/src/context/SessionContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useCallback } from 'react';
 import axios from 'axios';
 import API_URL from '../config';
 
